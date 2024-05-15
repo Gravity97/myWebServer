237ca94945dcae3ecbcebf89c961a5a46ad27e9d
Gravity97 <1842963193@qq.com>
2024-05-15 09:31:46 +0000

    完成线程池

--- src/pool/threadpool.cpp
+++ src/pool/threadpool.cpp
@@ -9,22 +9,21 @@
 ThreadPool::ThreadPool(size_t threadNum) : pool(std::make_shared<Pool>())
 {
     assert(threadNum > 0);
+    pool->isClose = false;
     for(size_t i = 0; i < threadNum; i++){
-        // start a new thread
+        // create a new thread
         std::thread([pool = this->pool]() {
-            std::unique_lock<std::mutex> locker(pool->mtx);
+            std::unique_lock<std::mutex> locker(pool->mtx); // if unique_lock can't get the lock, it will wait
             while(true){
                 if(!pool->tasks.empty()){
                     auto task = std::move(pool->tasks.front());
                     pool->tasks.pop();
-
-                    // start excuting the task
-                    locker.unlock();
-                    task();
+                    locker.unlock(); // other thread can access task queue
+                    task(); // start excuting the task
                     locker.lock();
                 } 
                 else if (pool->isClose) break;
-                else pool->cond.wait(locker);
+                else pool->cond.wait(locker); // wait() will unlock the mutex
             }
         }).detach();
     }
@@ -32,5 +31,22 @@ ThreadPool::ThreadPool(size_t threadNum) : pool(std::make_shared<Pool>())
 
 ThreadPool::~ThreadPool()
 {
-    
+    if(static_cast<bool>(pool)){
+        {
+            std::lock_guard<std::mutex> lock_guard(pool->mtx);
+            pool->isClose = true;
+        }
+        pool->cond.notify_all();
+    }
+}
+
+template<class F>
+void ThreadPool::addTask(F&& task)
+{
+    {
+        std::lock_guard<std::mutex> locker(pool->mtx);
+        pool->tasks.emplace(std::forward<F>(task));
+        // pool->tasks.emplace(std::forward(task));
+    }
+    pool->cond.notify_all();
 }
